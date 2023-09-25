948608e681446ab2eb2c6cedec18de5aa9fdb3c5
Thuongdo823 <145735595+Thuongdo823@users.noreply.github.com>
2023-09-26 00:56:56 +0700

    Delete BTTH01_CSE485_ex02 directory

--- BTTH01_CSE485_ex02/admin1/process.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    include("../connect/connect.php");
-    $sql = "SELECT * FROM theloai";
-    $stmt = $connect->prepare($sql);
-    $stmt->execute();
-    $result = $stmt->fetchAll();
-?>
