06734aea28138cb076bac4ba5a8775fd0105fcf8
Thuongdo823 <145735595+Thuongdo823@users.noreply.github.com>
2023-10-06 20:22:02 +0700

    Delete BTTH03_Patient directory

--- BTTH03_Patient/app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    define('APP_ROOT', dirname(__FILE__,3));
-    
-    define('DOMAIN', 'http://localhost/DoThiThuong_php/BTTH03_Patient');
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-    define('DB_NAME', 'phongmach');
-?>
