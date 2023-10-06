4d8d8f4407e38658b350cdc160d0759891809797
Thuongdo823 <145735595+Thuongdo823@users.noreply.github.com>
2023-10-06 20:19:53 +0700

    Delete BTTH1A/BTTH01_CSE485_ex01 directory

--- BTTH1A/BTTH01_CSE485_ex01/ex10.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    $arrs = ['1','b','c','d'];
-    for($i = 0; $i<count($arrs); $i++){
-        $arrs[$i] = strtoupper($arrs[$i]);
-    }
-    echo '<pre>';
-    print_r($arrs);
-    echo '</pre>';
-?>
--- BTTH1A/BTTH01_CSE485_ex01/ex11.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    $array = array(1,2,3,4,5);
-    unset($array['3']);
-    echo '<pre>';
-    print_r($array);
-    echo '</pre>';
-
-?>
