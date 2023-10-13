5aa4a23a201410d76f161830ca79f453ae9bd2e4
Thuongdo823 <thuongdo2k3@gmail.com>
2023-10-13 11:37:25 +0700

    up

---
+++ lara01/resources/views/categories/index.blade.php
@@ -0,0 +1,9 @@
+<!-- <div> -->
+    <!-- The best way to take care of the future is to take care of the present moment. -->
+    
+<!-- </div> -->
+
+
+@foreach ($categories as $category)
+    <p>{{$category->name}}</p>
+@endforeach
